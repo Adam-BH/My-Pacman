739d954032f95c0c7bb715737d227c3e11b2b8cd
Adam-BH <adam.bhouri@gmail.com>
2020-10-01 15:46:18 +0200

    1-Intro

---
+++ app_class.py
@@ -0,0 +1,38 @@
+import pygame, sys
+from settings import *
+
+pygame.init()
+
+vec = pygame.math.Vector2
+
+class App:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WIDTH, HIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.intro = 'intro'
+
+    def run(self):
+        while self.running:
+            if self.intro == 'intro':
+                self.intro_events()
+                self.intro_update()
+                self.intro_draw()
+            self.clock.tick(FPS)
+
+        pygame.quit()
+        sys.exit()
+
+###################### INTRO FUNCTIONS ######################
+
+    def intro_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+    def intro_update(self):
+        pass
+
+    def intro_draw(self):
+        pygame.display.update()
+
---
+++ settings.py
@@ -0,0 +1,8 @@
+#screen srttings
+WIDTH, HIGHT = 500, 700
+FPS = 60
+#color settings
+
+#player settings
+
+#mob settings
